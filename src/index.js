274eeebaf6583dc5250e17e3314dd0402befe941
Yuvraj Kaushik <harsh.kaushik0305@gmail.com>
2025-12-26 19:17:56 +0530

    Update index.js increased limit of file uploads

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 job.start();
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 app.use(cors())
 
 app.use("/api/auth",authRoutes)
@@ -19,4 +21,5 @@ app.use("/api/books",bookRoutes)
 app.listen(PORT, () => {
     console.log(`⚙️  Server is running on port: ${PORT}`)
     connectDB();
+
 })
